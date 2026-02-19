c267a0cad45b1ce13caea58e757e841a29c0abef
aishwarya <ashp17star@gmail.com>
2026-02-19 12:43:03 +0530

    .

--- 15_10_2025_MVVM_Demo/Screens/Products/View/ViewController.swift
+++ 15_10_2025_MVVM_Demo/Screens/Products/View/ViewController.swift
@@ -10,10 +10,52 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var productsTableView: UITableView!
+    let productViewModel = ProductViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+        initViewModel()
+        observeEvent()
+    }
+    
+    
+    func initViewModel(){
+        productViewModel.fetchProducts()
+    }
+    
+    func observeEvent(){
+        productViewModel.eventHandler = { [weak self] event in
+            switch event{
+                case .loading:
+                    print("Products Data Loading is in process!")
+                case .dataLoaded:
+                    print("Products Data Completely Loaded!")
+                    DispatchQueue.main.async {
+                        <#code#>
+                    }
+                case .stopLoading:
+                    print("Products Data Stopped loading...")
+            }
+        }
+    }
+}
+
+
+extension ViewController : UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+}
+
+
+extension ViewController : UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 }
--- 15_10_2025_MVVM_Demo/Screens/Products/ViewModel/ProductViewModel.swift
+++ 15_10_2025_MVVM_Demo/Screens/Products/ViewModel/ProductViewModel.swift
@@ -5,6 +5,30 @@
 //  Created by Vishal Jagtap on 18/02/26.
 //
 
+
+
 final class ProductViewModel{
+    var products : [Product] = []
+    var eventHandler : ((Event)->Void)?
     
+    func fetchProducts(){
+        APIManager.shared.getAllProducts { (result) in
+            switch result{
+            case .success(let newProductsArray):
+                self.products.append(contentsOf: newProductsArray)
+                self.eventHandler!(.dataLoaded)
+                
+            case .failure(let error):
+                self.eventHandler!(.stopLoading)
+            }
+        }
+    }
+}
+
+extension ProductViewModel{
+    enum Event{
+        case loading
+        case dataLoaded
+        case stopLoading
+    }
 }
